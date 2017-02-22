1b84c98ad9b5e093baefaf31e4dd437459adf178
Paraone <marcoswade@gmail.com>
2017-02-22 00:56:44 -0500

    add login and signup forms

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
@@ -6,12 +7,25 @@ const routes = require('./routes');
 // Set up the express app
 const app = express();
 
+// Set up session config
+app.use(session({
+	secret: 'Pa5sword!',
+	resave: false,
+	saveUninitialized: true,
+	cookie:{
+		secure: false
+	}
+}));
+
 // Log requests to the console.
 app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 
 app.use('/', routes);
 
--- web/app/pages/Home.js
+++ web/app/pages/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Login from '../components/Login';
 
 export default class Container extends Component {
 	constructor(props){
@@ -9,6 +10,7 @@ export default class Container extends Component {
 		return(
 			<div id="home_container">
 				<h1>Home Page</h1>
+				<Login />
 			</div>
 		)
 	}
